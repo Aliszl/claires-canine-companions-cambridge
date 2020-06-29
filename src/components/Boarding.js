7c2580dfe6ff1b4a44fd3e5ebd0cdd563b015aed
Alison Law <alisparklaw@gmail.com>
2020-06-29 15:18:09 +0100

    services wip

--- src/components/Boarding.js
+++ src/components/Boarding.js
@@ -5,23 +5,24 @@ const Boarding = () => {
         <StyledDiv>
           <h2>Boarding </h2>
        <p>All boarding is currently done in our homes, rather than kennels
-          so it is a very relaxed, homely environment. <br/> We will 
+          so it is a very relaxed, homely environment
+          <br/> <br/> £35 per 24 hours <br/>  <br/>We will 
           continue to offer one on one domestic boarding where that is the best fit, 
           but to extend our service we also have exciting future plans 
-          to develop a property, using our skills and knowledge about what
-           makes a happy dog and to extend our capacity.   
-           <br/>
-         This is a longtime dream and something of a passion project. 
+          to develop a property to extend our capacity, using our skills and knowledge about what
+           makes a happy dog.   
+           <br/> <br/>
+         This is a longtime lifestyle dream and something of a passion project. <br/>
           My partner and I, are purchasing a wonderful home,
           our own slice of Fenland countryside heaven, near Downham Market. 
-          We are developing a  doggy creche in 8 acres of land devoted to the dogs, 
-          meadows, a sensory garden, doggy pool, chill out rooms and socialising spaces, 
+          We are developing a  doggy creche nestled in 8 acres of land entirely devoted to dogs.
+          Meadows, a sensory garden, doggy pool, chill out rooms and socialising spaces, 
           doggy bedrooms with under floor heating and a groom room with a wash bath. 
-          
-          
-          We know how unsettling it can be to leave your furbabies,
+          <br/>
+          <br/>
+          We know how unsettling it can be to leave your furbabies in the care of another, they are family after all,
            so all areas will be covered by webcam so you can see what your dog is up to at all times. 
-          
+          We are confident enough in our skills and the service we provide to be entirely and radically open.
            </p>
         </StyledDiv>
       );
