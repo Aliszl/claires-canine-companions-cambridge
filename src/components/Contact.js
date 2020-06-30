0ef86ae3bfa555b3090b80c3720be20a40ef3682
Alison Law <alisparklaw@gmail.com>
2020-06-30 16:16:26 +0100

    email link

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,19 +6,30 @@ const Contact = () => {
     <StyledDiv>
       <div className="social-media-icons">
         <div className="top-row">
-        <a href="tel:+44-7971-489-600">
-          <i width="100%" class="fa fa-phone" aria-hidden="true">
-            {" "}
-            07971 489600
-          </i>{" "}
+          <a href="tel:+44-7971-489-600">
+            <i width="100%" class="fa fa-phone" aria-hidden="true">
+              {" "}
+              07971 489600
+            </i>{" "}
           </a>
           <br />
-          <a href="">
-
-          <i class="fa fa-envelope-o" aria-hidden="true">
-            {" "}
-            clairewalks@gmail.com
-          </i>
+          {/* <a
+            href="https://mail.google.com/mail/u/0/#inbox?compose=new?to:clairewalks@gmail.com"
+           
+          >
+            <i class="fa fa-envelope-o" aria-hidden="true">
+              {" "}
+              clairewalks@gmail.com
+            </i>
+          </a> */}
+          <a
+            href="mailto:clairewalks@gmail.com?bcc=alisparklaw@gmail.com&subject=Dog%Care%Query%20&body=Hi%20Claire,%0D%0DI%20have%20a%20Website%20Query%20Contact%20details:"
+           
+          >
+            <i class="fa fa-envelope-o" aria-hidden="true">
+              {" "}
+              clairewalks@gmail.com
+            </i>
           </a>
         </div>
         <div></div>
@@ -53,7 +64,6 @@ const Contact = () => {
           >
             blog
           </a>
-      
         </div>
         <div></div>
       </div>
