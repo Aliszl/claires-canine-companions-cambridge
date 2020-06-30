ebf6ca7d4f7fe8591695de1261b5fe8633733dfa
Alison Law <alisparklaw@gmail.com>
2020-06-30 14:57:18 +0100

    telephone links

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,48 +1,61 @@
 import React from "react";
 import { StyledDiv } from "../styles/style";
 
-
 const Contact = () => {
   return (
     <StyledDiv>
       <div className="social-media-icons">
-        <a
-          href="https://www.facebook.com/clairewalks/"
-          className="fa fa-facebook"
-        >
-          {" "}
-        </a>
-        <a
-          href="https://www.facebook.com/clairewalks/"
-          className="fa fa-twitter"
-        >
-          {" "}
-        </a>
-        <a
-          href="https://www.linkedin.com/in/claire-bloomfield-8434b016b/?originalSubdomain=uk"
-          className="fa fa-linkedin"
-        >
-          {" "}
-        </a>
-        <a
-          href="https://www.linkedin.com/in/claire-bloomfield-8434b016b/?originalSubdomain=uk"
-          className="fa fa-whatsapp"
-        >
-          {" "}
-        </a>
-        <a href="https://clairebloomfield.blogspot.com/" className="fa fa-link">blog</a>
-        <i width="100%" class="fa fa-phone" aria-hidden="true">
-          {" "}
-          07971 489600
-        </i>{" "}
-        <br />
-        <i class="fa fa-envelope-o" aria-hidden="true">
-          {" "}
-          clairewalks@gmail.com
-        </i>
-      </div>
-      <div>
+        <div className="top-row">
+        <a href="tel:+44-7971-489-600">
+          <i width="100%" class="fa fa-phone" aria-hidden="true">
+            {" "}
+            07971 489600
+          </i>{" "}
+          </a>
+          <br />
+          <a href="">
+
+          <i class="fa fa-envelope-o" aria-hidden="true">
+            {" "}
+            clairewalks@gmail.com
+          </i>
+          </a>
+        </div>
+        <div></div>
+        <div className="bottom-row">
+          <a
+            href="https://www.facebook.com/clairewalks/"
+            className="fa fa-facebook"
+          >
+            {" "}
+          </a>
+          <a
+            href="https://www.facebook.com/clairewalks/"
+            className="fa fa-twitter"
+          >
+            {" "}
+          </a>
+          <a
+            href="https://www.linkedin.com/in/claire-bloomfield-8434b016b/?originalSubdomain=uk"
+            className="fa fa-linkedin"
+          >
+            {" "}
+          </a>
+          <a
+            href="https://www.linkedin.com/in/claire-bloomfield-8434b016b/?originalSubdomain=uk"
+            className="fa fa-whatsapp"
+          >
+            {" "}
+          </a>
+          <a
+            href="https://clairebloomfield.blogspot.com/"
+            className="fa fa-link"
+          >
+            blog
+          </a>
       
+        </div>
+        <div></div>
       </div>
     </StyledDiv>
   );
